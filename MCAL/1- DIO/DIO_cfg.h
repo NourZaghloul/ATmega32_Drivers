56601917e81e9c406d91066fd36699885a50fac9
nour <nzaghloul37@gmail.com>
2024-06-06 20:41:00 +0300

    Adding DIO Driver files

---
+++ MCAL/1- DIO/DIO_cfg.h
@@ -0,0 +1,13 @@
+/*
+ * DIO_cfg.h
+ *
+ *  Created on: May 2, 2024
+ *      Author: Nour Zaghloul
+ */
+
+#ifndef DIO_CFG_H_
+#define DIO_CFG_H_
+
+
+
+#endif /* DIO_CFG_H_ */
---
+++ MCAL/1- DIO/DIO_prv.h
@@ -0,0 +1,13 @@
+/*
+ * DIO_prv.h
+ *
+ *  Created on: May 2, 2024
+ *      Author: Nour Zaghloul
+ */
+
+#ifndef DIO_PRV_H_
+#define DIO_PRV_H_
+
+
+
+#endif /* DIO_PRV_H_ */
